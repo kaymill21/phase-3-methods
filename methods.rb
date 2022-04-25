9187fb811faf50ae1ca1d0c3336c703ccaf06612
kayla <kay.ann2000@gmail.com>
2022-04-25 11:00:22 -0600

    commit tree d96163e9e69ac80bf5613764e0bc1630ef0448b5

--- methods.rb
+++ methods.rb
@@ -1,1 +1,21 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+#greet that takes one argument, a name. It should output the string "Hello, name!" (with "name" being whatever value was passed as an argument) to the terminal with #puts.
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "programmer") 
+    puts "Hello, #{name}!" 
+end
+
+def add(num1, num2)
+    num1 + num2
+end
+
+def halve(num1)
+   return nil unless num1.class == Integer
+        num1/2
+    end
